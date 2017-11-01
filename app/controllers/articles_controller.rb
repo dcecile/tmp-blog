f408374c25c4167fa08f445ff0ce86d4088263e4
Dan Cecile <dancecile@gmail.com>
2017-11-01 12:12:22 -0400

    Set up Article create/update/destroy operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,40 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "The article '#{@article.title}' is newly created!"
+    redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "The article '#{@article.title}' is updated"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "The article '#{@article.title}' has been destroyed..."
+    redirect_to articles_path
   end
 end
